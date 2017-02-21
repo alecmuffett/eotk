dbc6c2d9ed5450d2ce7146d6a9c390f35f781500
Alec Muffett <alec.muffett@gmail.com>
2017-02-21 21:46:07 +0000

    commit: script more

---
+++ opt.d/install-onionbalance-on-debian.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -x
+
+here=`dirname $0`
+cd $here || exit 1
+here=`pwd`
+
+# install onionbalance
+sudo easy_install onionbalance # also warms-up sudo
+
+# install socat for log review
+sudo aptitude install socat
+
+# check: this should print `/usr/local/bin/onionbalance`
+sudo which onionbalance
+
+# fix/propagate the owner's read-bit
+sudo find /usr/local/bin /usr/local/lib -perm -0400 -print0 | sudo xargs -0 chmod a+r
+
+# fix/propagate the owner's execute-bit
+sudo find /usr/local/bin /usr/local/lib -perm -0100 -print0 | sudo xargs -0 chmod a+x
+
+echo this should say: onionbalance 0.1.7 -- or higher
+onionbalance --version
+
+exit 0
