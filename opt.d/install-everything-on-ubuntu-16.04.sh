3cf5b9e1428b31443166b68c22842235acbcda2a
Alec Muffett <alec.muffett@gmail.com>
2017-02-27 11:46:15 +0000

    commit: disable tor autostart

--- opt.d/install-everything-on-ubuntu-16.04.sh
+++ opt.d/install-everything-on-ubuntu-16.04.sh
@@ -6,13 +6,14 @@ deb-src http://deb.torproject.org/torproject.org xenial main
 EOF
 
 gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 || exit 1
-
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add - || exit 1
 
 sudo apt-get --yes update || exit 1
-
 sudo apt-get --yes install tor deb.torproject.org-keyring socat python-pip || exit 1
+sudo systemctl stop tor # is there a way to install-without-enable?
+sudo systemctl disable tor # we don't need the system to run it
 
+echo ""
 echo $0: if you are already running a webserver then nginx will whine about port80, do not worry
 sudo apt-get --yes install nginx-extras
 sudo systemctl stop nginx # is there a way to install-without-enable?
@@ -20,11 +21,10 @@ sudo systemctl disable nginx # we don't need the system to run it
 
 sudo find /var/log/nginx/ -type f -perm -0200 -print0 | sudo xargs -0 chmod g+w || exit 1
 
+echo ""
 echo $0: this will probably whine about pip versions, do not worry about it.
 sudo pip install onionbalance || exit 1
-
 sudo find /usr/local/bin /usr/local/lib -perm -0400 -print0 | sudo xargs -0 chmod a+r || exit 1
-
 sudo find /usr/local/bin /usr/local/lib -perm -0100 -print0 | sudo xargs -0 chmod a+x || exit 1
 
 exit 0
