fa0e83aa06419376a110d017d326af296b630685
Alec Muffett <alec.muffett@gmail.com>
2017-02-27 10:07:19 +0000

    commit: error checks, fix sudo

--- opt.d/install-everything-on-ubuntu-16.04.sh
+++ opt.d/install-everything-on-ubuntu-16.04.sh
@@ -5,20 +5,20 @@ deb http://deb.torproject.org/torproject.org xenial main
 deb-src http://deb.torproject.org/torproject.org xenial main
 EOF
 
-gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
+gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 || exit 1
 
-gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
+gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add - || exit 1
 
-sudo apt-get update
+sudo apt-get update || exit 1
 
-apt-get install tor deb.torproject.org-keyring nginx-extras socat python-pip
+sudo apt-get install tor deb.torproject.org-keyring nginx-extras socat python-pip || exit 1
 
-sudo find /var/log/nginx/ -type f -perm -0200 -print0 | sudo xargs -0 chmod g+w
+sudo find /var/log/nginx/ -type f -perm -0200 -print0 | sudo xargs -0 chmod g+w || exit 1
 
-sudo pip install onionbalance
+sudo pip install onionbalance || exit 1
 
-sudo find /usr/local/bin /usr/local/lib -perm -0400 -print0 | sudo xargs -0 chmod a+r
+sudo find /usr/local/bin /usr/local/lib -perm -0400 -print0 | sudo xargs -0 chmod a+r || exit 1
 
-sudo find /usr/local/bin /usr/local/lib -perm -0100 -print0 | sudo xargs -0 chmod a+x
+sudo find /usr/local/bin /usr/local/lib -perm -0100 -print0 | sudo xargs -0 chmod a+x || exit 1
 
 exit 0
