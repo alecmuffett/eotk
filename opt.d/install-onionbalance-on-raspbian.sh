fe1c2d646b9295fde27fe023c371ab6e5eddd1af
Alec Muffett <alec.muffett@gmail.com>
2017-03-03 22:08:56 +0000

    commit: rpi easy_install fix

--- opt.d/install-onionbalance-on-raspbian.sh
+++ opt.d/install-onionbalance-on-raspbian.sh
@@ -5,18 +5,16 @@ cd $here || exit 1
 here=`pwd`
 
 # install socat for log review
-sudo aptitude install socat python-setuptools
+sudo aptitude install socat python-pip || exit 1
 
 # install onionbalance
-sudo easy_install onionbalance # also warms-up sudo
+sudo pip install onionbalance || exit 1
 
 # check: this should print `/usr/local/bin/onionbalance`
 sudo which onionbalance
 
-# fix/propagate the owner's read-bit
+# fix/propagate the owner's read/execute-bits
 sudo find /usr/local/bin /usr/local/lib -perm -0400 -print0 | sudo xargs -0 chmod a+r
-
-# fix/propagate the owner's execute-bit
 sudo find /usr/local/bin /usr/local/lib -perm -0100 -print0 | sudo xargs -0 chmod a+x
 
 echo this should say: onionbalance 0.1.7 -- or higher
