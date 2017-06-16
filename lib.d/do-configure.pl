b4993fc47a5a4c372cc114414e3348e4b8e580bc
Alec Muffett <alec.muffett@gmail.com>
2017-06-16 13:49:20 +0100

    commit: fix preservation of email

--- lib.d/do-configure.pl
+++ lib.d/do-configure.pl
@@ -404,7 +404,8 @@ sub DoProject {
 &SetEnv("extra_processing_csv", "");
 
 &SetEnv("preserve_csv", "");
-&SetEnv("preserve_cookie", "-~=~=~-");
+&SetEnv("preserve_preamble", "[>@\\\\s]");
+&SetEnv("preserve_cookie", "~-=~=-~");
 
 &SetEnv("SCRIPT_NAMES", "bounce.sh debugoff.sh debugon.sh harvest.sh maps.sh nxreload.sh start.sh status.sh stop.sh syntax.sh torreload.sh");
 &SetEnv("SCRIPT_PAUSE", 5);
