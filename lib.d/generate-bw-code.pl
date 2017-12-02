250a6513bc76d11e44c5cbbf6e16cddef3d223a8
Alec Muffett <alec.muffett@gmail.com>
2017-12-02 11:12:59 +0000

    commit: generator version 1

--- lib.d/generate-bw-code.pl
+++ lib.d/generate-bw-code.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/perl
 
 $warning = "(generated)";
+$begin = "# ---- BEGIN GENERATED CODE ---- -*- awk -*-\n\n";
+$end = "# ---- END GENERATED CODE ----\n";
+
 $indent = "  ";
 @polite = ();
 @black = ();
@@ -24,43 +27,43 @@ while (<DATA>) {
         $flag = "\$non_whitelist_${lc_what}";
 
         push(@black, "%%IF %$uc_bl%\n");
-        push(@black, "# check $lc_bl\n");
+        push(@black, "# check $lc_bl $warning\n");
         push(@black, "%%CSV %$uc_bl%\n");
         push(@black, "$condition { %NGINX_ACTION_ABORT%; }\n");
         push(@black, "%%ENDCSV\n");
         push(@black, "%%ELSE\n");
-        push(@black, "# no $lc_bl\n");
+        push(@black, "# no $lc_bl $warning\n");
         push(@black, "%%ENDIF\n");
         push(@black, "\n");
 
         push(@white, "%%IF %$uc_wl%\n");
-        push(@white, "# check $lc_wl\n");
+        push(@white, "# check $lc_wl $warning\n");
         push(@white, "set $flag 1;\n");
         push(@white, "%%CSV %$uc_wl%\n");
         push(@white, "$condition { set $flag 0; }\n");
         push(@white, "%%ENDCSV\n");
         push(@white, "%%ELSE\n");
-        push(@white, "# no $lc_wl\n");
+        push(@white, "# no $lc_wl $warning\n");
         push(@white, "%%ENDIF\n");
         push(@white, "\n");
 
         push(@tail, "%%IF %$uc_wl%\n");
-        push(@tail, "# check success of $lc_wl\n");
+        push(@tail, "# check success of $lc_wl $warning\n");
         push(@tail, "if ( $flag ) { %NGINX_ACTION_ABORT%; }\n");
         push(@tail, "%%ELSE\n");
-        push(@tail, "# no check for success of $lc_wl\n");
+        push(@tail, "# no check for success of $lc_wl $warning\n");
         push(@tail, "%%ENDIF\n");
         push(@tail, "\n");
     }
     elsif ($how eq "block") {
         push(@polite, "%%IF %$uc_what%\n");
-        push(@polite, "# polite block for $lc_what\n");
+        push(@polite, "# polite block for $lc_what $warning\n");
         push(@polite, "%%CSV %$uc_what%\n");
         push(@polite, "$condition { return 403 \"%BLOCK_ERR%\"; }\n");
         push(@polite, "%%ENDCSV\n");
 
         push(@polite, "%%ELSE\n");
-        push(@polite, "# no polite block for $lc_what\n");
+        push(@polite, "# no polite block for $lc_what $warning\n");
         push(@polite, "%%ENDIF\n");
         push(@polite, "\n");
     }
@@ -69,30 +72,37 @@ while (<DATA>) {
     }
 }
 
-print "# polite blocks\n\n";
+open(OUT, ">nginx-generated-blocks.conf") || die;
+print OUT $indent x 2, $begin;
+print OUT $indent x 2, "# polite blocks $warning\n\n";
 foreach $x (@polite) {
-    print $x;
+    print OUT $indent x 2, $x;
 }
-print "\n";
+print OUT "\n";
 
-print "# blacklists\n\n";
+print OUT $indent x 2, "# blacklists $warning\n\n";
 foreach $x (@black) {
-    print $x;
+    print OUT $indent x 2, $x;
 }
-print "\n";
+print OUT "\n";
 
-print "# whitelists\n\n";
+print OUT $indent x 2, "# whitelists $warning\n\n";
 foreach $x (@white) {
-    print $x;
+    print OUT $indent x 2, $x;
 }
-print "\n";
+print OUT "\n";
+print OUT $indent x 2, $end;
+close(OUT);
 
-print "# whitelist checks\n\n";
+open(OUT, ">nginx-generated-checks.conf") || die;
+print OUT $indent x 3, $begin;
+print OUT $indent x 3, "# whitelist checks $warning\n\n";
 foreach $x (@tail) {
-    print $x;
+    print OUT $indent x 3, $x;
 }
-print "\n";
-
+print OUT "\n";
+print OUT $indent x 3, $end;
+close(OUT);
 
 # NB: AVOID `location` DIRECTIVE IN THE CONDITIONALS, BECAUSE IT
 # TRIGGERS A HANDLER...
