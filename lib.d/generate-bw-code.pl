625e106fd9466244d69825fe352e8bc7ff56289b
Alec Muffett <alec.muffett@gmail.com>
2017-12-02 09:40:40 +0000

    commit: update codegen to take a switch

--- lib.d/generate-bw-code.pl
+++ lib.d/generate-bw-code.pl
@@ -1,50 +1,59 @@
 #!/usr/bin/perl
 
+$warning = "(generated code)";
+
 @black = ();
 @white = ();
 @tail = ();
 
 while (<DATA>) {
+    next if /^#/;
     next if /^\s*$/;
+
     chomp;
     s/\s+/ /g;
-    ($lc_what, $condition) = split(/\s+/, $_, 2);
-    $uc_what = uc($lc_what);
+    ($how, $lc_what, $condition) = split(/\s+/, $_, 3);
 
-    $uc_bl = "${uc_what}_BLACKLIST_RE";
-    $uc_wl = "${uc_what}_WHITELIST_RE";
-    $lc_bl = lc($uc_bl);
-    $lc_wl = lc($uc_wl);
-    $flag = "\$non_whitelist_${lc_what}";
+    if ($how eq "bwlist") {
+        $uc_what = uc($lc_what);
+        $uc_bl = "${uc_what}_BLACKLIST_RE";
+        $uc_wl = "${uc_what}_WHITELIST_RE";
+        $lc_bl = lc($uc_bl);
+        $lc_wl = lc($uc_wl);
+        $flag = "\$non_whitelist_${lc_what}";
 
-    push(@black, "%%IF %$uc_bl%\n");
-    push(@black, "# check $lc_bl\n");
-    push(@black, "%%CSV $uc_bl\n");
-    push(@black, "$condition { %NGINX_ACTION_ABORT%; }\n");
-    push(@black, "%%ENDCSV\n");
-    push(@black, "%%ELSE\n");
-    push(@black, "# no $lc_bl\n");
-    push(@black, "%%ENDIF\n");
-    push(@black, "\n");
+        push(@black, "%%IF %$uc_bl%\n");
+        push(@black, "# check $lc_bl $warning\n");
+        push(@black, "%%CSV $uc_bl\n");
+        push(@black, "$condition { %NGINX_ACTION_ABORT%; }\n");
+        push(@black, "%%ENDCSV\n");
+        push(@black, "%%ELSE\n");
+        push(@black, "# no $lc_bl $warning\n");
+        push(@black, "%%ENDIF\n");
+        push(@black, "\n");
 
-    push(@white, "%%IF %$uc_wl%\n");
-    push(@white, "# check $lc_wl\n");
-    push(@white, "set $flag 1;\n");
-    push(@white, "%%CSV %$uc_wl%\n");
-    push(@white, "$condition { set $flag 0; }\n");
-    push(@white, "%%ENDCSV\n");
-    push(@white, "%%ELSE\n");
-    push(@white, "# no $lc_wl\n");
-    push(@white, "%%ENDIF\n");
-    push(@white, "\n");
+        push(@white, "%%IF %$uc_wl%\n");
+        push(@white, "# check $lc_wl $warning\n");
+        push(@white, "set $flag 1;\n");
+        push(@white, "%%CSV %$uc_wl%\n");
+        push(@white, "$condition { set $flag 0; }\n");
+        push(@white, "%%ENDCSV\n");
+        push(@white, "%%ELSE\n");
+        push(@white, "# no $lc_wl $warning\n");
+        push(@white, "%%ENDIF\n");
+        push(@white, "\n");
 
-    push(@tail, "%%IF %$uc_wl%\n");
-    push(@tail, "# check success of $lc_wl\n");
-    push(@tail, "if ( $flag ) { %NGINX_ACTION_ABORT%; }\n");
-    push(@tail, "%%ELSE\n");
-    push(@tail, "# no check for success of $lc_wl\n");
-    push(@tail, "%%ENDIF\n");
-    push(@tail, "\n");
+        push(@tail, "%%IF %$uc_wl%\n");
+        push(@tail, "# check success of $lc_wl $warning\n");
+        push(@tail, "if ( $flag ) { %NGINX_ACTION_ABORT%; }\n");
+        push(@tail, "%%ELSE\n");
+        push(@tail, "# no check for success of $lc_wl $warning\n");
+        push(@tail, "%%ENDIF\n");
+        push(@tail, "\n");
+    }
+    else {
+        die "bad config line at line $.: $_\n";
+    }
 }
 
 
@@ -58,8 +67,9 @@ foreach $x (@white) {
     print "    ", $x;
 }
 
-print "----------------\n";
+print "----------------\n\n";
 
+print "      # whitelist checks\n";
 foreach $x (@tail) {
     print "      ", $x;
 }
@@ -69,7 +79,8 @@ foreach $x (@tail) {
 # TRIGGERS A HANDLER...
 __END__;
 
-user_agent if ( $http_user_agent ~* "%0%" )
-referer if ( $http_referer ~* "%0%" )
-host if ( $http_host ~* "%0%" )
-path if ( $uri ~* "%0%" )
+# blacklists and whitelists
+bwlist user_agent if ( $http_user_agent ~* "%0%" )
+bwlist referer if ( $http_referer ~* "%0%" )
+bwlist host if ( $http_host ~* "%0%" )
+bwlist path if ( $uri ~* "%0%" )
