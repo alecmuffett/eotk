822fcb73271a8518d2e0b8c2c58a4e906a31ea5f
Alec Muffett <alec.muffett@gmail.com>
2019-11-10 14:03:54 +0000

    commit: work in progress

--- lib.d/explain.pl
+++ lib.d/explain.pl
@@ -1,13 +1,15 @@
 #!/usr/bin/perl -n
 next unless /##/o;
+
 s/\s+/ /go;
 s/^\s//;
 s/\s$//;
 
 ($opts, $docs) = split(/\s*##\s*/);
 $opts =~ s/x--/--/g;
-$opts =~ s/\*//g;
 $opts =~ s/\)$//;
+
+$key = $opts;
 
 if ($opts =~ /\|/) {
     $opts = "<$opts>";
@@ -20,4 +22,10 @@ if ($docs =~ /\|/) {
     $args = '';
 }
 
-print "  $ARGV $opts$args\n    $docs\n";
+$foo{$key} = "  $ARGV $opts$args\n    $docs\n\n";
+
+END {
+    foreach $key (sort keys %foo) {
+        print $foo{$key};
+    }
+}
