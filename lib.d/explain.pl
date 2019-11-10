2a7c5d0e0a2d9133a9c96cacbfefec2f42e42c6c
Alec Muffett <alec.muffett@gmail.com>
2019-11-10 13:42:51 +0000

    commit: first cut

---
+++ lib.d/explain.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -n
+next unless /##/o;
+s/\s+/ /go;
+s/^\s//;
+s/\s$//;
+
+($opts, $docs) = split(/\s*##\s*/);
+$opts =~ s/x--/--/g;
+$opts =~ s/\*//g;
+$opts =~ s/\)$//;
+
+if ($opts =~ /\|/) {
+    $opts = "<$opts>";
+}
+
+if ($docs =~ /\|/) {
+    ($args, $docs) = split(/\s*\|\s*/, $docs);
+    $args = " $args";
+} else {
+    $args = '';
+}
+
+print "    $ARGV $opts$args\n\t$docs\n";
