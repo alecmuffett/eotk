dd8decc7fa31ea4f7bce07955c3bd72c4083c259
Alec Muffett <alec.muffett@gmail.com>
2020-06-08 19:36:12 +0000

    commit: make shippable, hopefully

--- opt.d/build-nginx-on-raspbian-stretch.sh
+++ opt.d/build-nginx-on-raspbian-stretch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -x
 tool=openresty
-tool_version=1.15.8.3 # All the releases are signed by the public PGP key A0E98066 of Yichun Zhang.
+tool_version=1.15.8.3 # "All the releases are signed by the public PGP key A0E98066 of Yichun Zhang."
+tool_signing_key=25451EB088460026195BD62CB550E09EA0E98066 # this is the full key signature
+keyserver=keyserver.ubuntu.com
 MODS="https://github.com/yaoweibin/ngx_http_substitutions_filter_module.git"
 OPTS="--with-http_sub_module"
 
@@ -10,8 +12,8 @@ cd $opt_dir || exit 1
 opt_dir=`pwd`
 install_dir=$opt_dir/$tool # have to bury the tool because of name clashes
 
-# dependencies # CHANGE THIS
-# sudo aptitude install -y libpcre3-dev zlib1g-dev libssl1.0.2 libssl1.0-dev dirmngr || exit 1
+# dependencies # todo: review/update this
+sudo aptitude install -y libpcre3-dev zlib1g-dev libssl1.0.2 libssl1.0-dev dirmngr || exit 1
 
 # get OpenResty and cd into it
 tool_url=https://openresty.org/download/openresty-$tool_version.tar.gz
@@ -21,8 +23,8 @@ tool_sig=`basename "$tool_sig_url"`
 tool_dir=`basename "$tool_tarball" .tar.gz`
 test -f "$tool_tarball" || curl -o "$tool_tarball" "$tool_url" || exit 1
 test -f "$tool_sig" || curl -o "$tool_sig" "$tool_sig_url" || exit 1
-#gpg --keyserver hkp://$keyserver:80 --recv-keys $ngxsigningkey || exit 1
-#gpg --verify $ngxsig || exit 1
+gpg --keyserver hkp://$keyserver:80 --recv-keys $tool_signing_key || exit 1
+gpg --verify $tool_sig || exit 1
 test -d "$tool_dir" || tar zxf "$tool_tarball" || exit 1
 
 # build dir
@@ -42,11 +44,13 @@ for modurl in $MODS ; do
     MODADD="$MODADD --add-module=$src_dir/$moddir"
 done
 
+# stale ARM options from old LuaJIT, in case they are needed ever again
+# --with-cc-opt="-fPIE -fstack-protector-strong -fexceptions -D_FORTIFY_SOURCE=2" \
+# --with-ld-opt="-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now -Wl,-rpath,$LUAJIT_LIB" \
+
 # configure and build
 echo "$0: info: you can ignore any 'unrecognized command line -msse4.2' error"
 env ./configure --prefix=$install_dir $OPTS $MODADD || exit 1
-# --with-cc-opt="-fPIE -fstack-protector-strong -fexceptions -D_FORTIFY_SOURCE=2" \
-# --with-ld-opt="-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now -Wl,-rpath,$LUAJIT_LIB" \
 make || exit 1
 make install || exit 1
 
@@ -55,7 +59,7 @@ cd $opt_dir || exit 1
 ln -sf $tool/nginx/sbin/nginx || exit 1
 
 # cleanup
-: rm -rf $tool_tarball $tool_sig $tool_dir
+rm -rf $tool_tarball $tool_sig $tool_dir
 
 # done
 exit 0
