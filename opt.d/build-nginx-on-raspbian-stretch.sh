f5f225a44d8e8726ded8e9d555326a18401ed11c
Alec Muffett <alec.muffett@gmail.com>
2020-06-08 22:10:58 +0000

    syncronise build scripts

--- opt.d/build-nginx-on-raspbian-stretch.sh
+++ opt.d/build-nginx-on-raspbian-stretch.sh
@@ -1,8 +1,13 @@
 #!/bin/sh -x
-tool=openresty
-tool_version=1.15.8.3 # "All the releases are signed by the public PGP key A0E98066 of Yichun Zhang."
+tool=openresty # "All the releases are signed by the public PGP key A0E98066 of Yichun Zhang."
+tool_version=1.15.8.3
 tool_signing_key=25451EB088460026195BD62CB550E09EA0E98066 # this is the full key signature
+tool_url=https://openresty.org/download/openresty-$tool_version.tar.gz
+tool_sig_url=https://openresty.org/download/openresty-1.15.8.3.tar.gz.asc
+apt_deps="libpcre3-dev zlib1g-dev libssl1.0.2 libssl1.0-dev dirmngr"
+sub_path="nginx/sbin/nginx"
 keyserver=keyserver.ubuntu.com
+
 MODS="https://github.com/yaoweibin/ngx_http_substitutions_filter_module.git"
 OPTS="--with-http_sub_module"
 
@@ -10,14 +15,12 @@ OPTS="--with-http_sub_module"
 opt_dir=`dirname $0`
 cd $opt_dir || exit 1
 opt_dir=`pwd`
-install_dir=$opt_dir/$tool # have to bury the tool because of name clashes
+install_dir=$opt_dir/$tool.d
 
-# dependencies # todo: review/update this
-sudo aptitude install -y libpcre3-dev zlib1g-dev libssl1.0.2 libssl1.0-dev dirmngr || exit 1
+# dependencies
+sudo aptitude install -y $apt_deps || exit 1
 
-# get OpenResty and cd into it
-tool_url=https://openresty.org/download/openresty-$tool_version.tar.gz
-tool_sig_url=https://openresty.org/download/openresty-1.15.8.3.tar.gz.asc
+# get $tool and cd into it
 tool_tarball=`basename "$tool_url"`
 tool_sig=`basename "$tool_sig_url"`
 tool_dir=`basename "$tool_tarball" .tar.gz`
@@ -29,8 +32,6 @@ test -d "$tool_dir" || tar zxf "$tool_tarball" || exit 1
 
 # build dir
 cd $tool_dir || exit 1
-src_dir=`pwd`
-cd $src_dir || exit 1
 
 # get the mods
 MODADD=""
@@ -41,7 +42,7 @@ for modurl in $MODS ; do
     else
         git clone $modurl || exit 1
     fi
-    MODADD="$MODADD --add-module=$src_dir/$moddir"
+    MODADD="$MODADD --add-module=$tool_dir/$moddir"
 done
 
 # stale ARM options from old LuaJIT, in case they are needed ever again
@@ -56,7 +57,7 @@ make install || exit 1
 
 # link the binary for EOTK access ($opt_dir is in $PATH)
 cd $opt_dir || exit 1
-ln -sf $tool/nginx/sbin/nginx || exit 1
+ln -sf $install_dir/$sub_path || exit 1
 
 # cleanup
 rm -rf $tool_tarball $tool_sig $tool_dir
--- opt.d/build-tor-on-raspbian-stretch.sh
+++ opt.d/build-tor-on-raspbian-stretch.sh
@@ -1,38 +1,46 @@
 #!/bin/sh -x
-
+tool=tor
+tool_version=0.4.3.5
+tool_signing_key="6AFEE6D49E92B601 C218525819F78451"
+tool_url=https://dist.torproject.org/tor-$tool_version.tar.gz
+tool_sig_url=https://dist.torproject.org/tor-$tool_version.tar.gz.asc
+apt_deps="libevent-dev zlib1g-dev libssl1.0.2 libssl1.0-dev dirmngr"
+sub_path="bin/$tool"
 keyserver=keyserver.ubuntu.com
-torversion=0.4.3.5
-torsigningkeys="6AFEE6D49E92B601 C218525819F78451"
 
-here=`dirname $0`
-cd $here || exit 1
-here=`pwd`
+# install dir
+opt_dir=`dirname $0`
+cd $opt_dir || exit 1
+opt_dir=`pwd`
+install_dir=$opt_dir/$tool.d
 
-sudo aptitude install -y libevent-dev zlib1g-dev libssl1.0.2 libssl1.0-dev dirmngr || exit 1
+# dependencies
+sudo aptitude install -y $apt_deps || exit 1
 
-torurl=https://dist.torproject.org/tor-$torversion.tar.gz
-torsigurl=https://dist.torproject.org/tor-$torversion.tar.gz.asc
-torfile=`basename $torurl`
-torsig=`basename $torsigurl`
-tordir=`basename $torurl .tar.gz`
+# get $tool and cd into it
+tool_tarball=`basename "$tool_url"`
+tool_sig=`basename "$tool_sig_url"`
+tool_dir=`basename "$tool_tarball" .tar.gz`
+test -f "$tool_tarball" || curl -o "$tool_tarball" "$tool_url" || exit 1
+test -f "$tool_sig" || curl -o "$tool_sig" "$tool_sig_url" || exit 1
+gpg --keyserver hkp://$keyserver:80 --recv-keys $tool_signing_key || exit 1
+gpg --verify $tool_sig || exit 1
+test -d "$tool_dir" || tar zxf "$tool_tarball" || exit 1
 
-test -f $torfile || curl -o $torfile $torurl || exit 1
-test -f $torsig || curl -o $torsig $torsigurl || exit 1
-gpg --keyserver hkp://$keyserver:80 --recv-keys $torsigningkeys || exit 1
-gpg --verify $torsig || exit 1
-test -d $tordir || tar zxf $torfile || exit 1
+# build dir
+cd $tool_dir || exit 1
 
-cd $tordir || exit 1
-
-./configure --prefix=$here || exit 1
-
+# configure and build
+./configure --prefix=$install_dir || exit 1
 make || exit 1
 make install || exit 1
 
-cd $here || exit 1
+# link the binary for EOTK access ($opt_dir is in $PATH)
+cd $opt_dir || exit 1
+ln -sf $install_dir/$sub_path || exit 1
 
-ln -sf bin/tor || exit 1
+# cleanup
+rm -rf $tool_tarball $tool_sig $tool_dir
 
-rm -rf $torfile $torsig $tordir
-
+# done
 exit 0
