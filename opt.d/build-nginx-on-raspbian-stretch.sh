ceaa47aaab70d19bbf46c53801b1fd7ae1f8bc28
Alec Muffett <alec.muffett@gmail.com>
2020-06-09 10:14:22 +0000

    commit: new build template

--- opt.d/build-nginx-on-raspbian-stretch.sh
+++ opt.d/build-nginx-on-raspbian-stretch.sh
@@ -1,15 +1,15 @@
 #!/bin/sh -x
 
-# stdlib
+# platform-independent lib.sh
 opt_dir=`dirname $0`
 opt_dir=`pwd`
-. $opt_dir/build.d/lib.sh
+. $opt_dir/lib.sh
 
-# satisfy dependencies
+# platform dependencies
 apt_deps="libpcre3-dev zlib1g-dev libssl1.0.2 libssl1.0-dev dirmngr"
 sudo aptitude install -y $apt_deps || exit 1
 
-# go
+# platform-independent build (see lib.sh)
 SetupOpenRestyVars || exit 1
 CustomiseVars || exit 1
 SetupForBuild || exit 1
