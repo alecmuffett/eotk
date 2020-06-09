a521b84667ea16079da9b37c55bc847870d69d3c
Alec Muffett <alec.muffett@gmail.com>
2020-06-09 21:35:37 +0100

    commit: add portsnap comment

--- opt.d/build-freebsd-12.1.sh
+++ opt.d/build-freebsd-12.1.sh
@@ -5,6 +5,19 @@ cd `dirname $0` || exit 1
 opt_dir=`pwd`
 . lib.sh || exit 1
 
+# FreeBSD users: note: you may want to install libzstd to make
+# compression go faster; apparently the way that you do this is to do
+# something like:
+#
+#  portsnap fetch && portsnap extract
+#  cd /usr/ports/archivers/zstd && make install
+#
+# If you want to do that before re-running this script, go right
+# ahead. Portsnap seems to involve a lot of work, and it complains
+# about the `pkg` version of `gmake`, plus it is optional / it is
+# possible to carry on without libzstd if you are just experimenting
+# with EOTK.  Hence I have not bothered.
+
 # platform dependencies
 shared_deps="gmake libevent"
 echo $0: calling su to satisfy package dependencies
