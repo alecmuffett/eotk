b8b8ec6a6b1c5fe850a3c13e7094a4f0b2d2fbc2
Alec Muffett <alec.muffett@gmail.com>
2020-07-23 18:45:58 +0000

    commit: build-ubuntu-20.04.sh

---
+++ opt.d/build-ubuntu-20.04.sh
@@ -0,0 +1,34 @@
+#!/bin/sh -x
+
+# platform-independent lib.sh
+cd `dirname $0` || exit 1
+opt_dir=`pwd`
+. lib.sh || exit 1
+
+# platform dependencies
+shared_deps="
+dirmngr
+libevent-dev
+libpcre3-dev
+libssl-dev
+libssl1.1
+zlib1g-dev
+"
+sudo apt install -y $shared_deps || exit 1
+
+# build openresty
+SetupOpenRestyVars || exit 1
+CustomiseVars || exit 1
+SetupForBuild || exit 1
+ConfigureOpenResty || exit 1
+BuildAndCleanup || exit 1
+
+# build tor
+SetupTorVars || exit 1
+CustomiseVars || exit 1
+SetupForBuild || exit 1
+ConfigureTor || exit 1
+BuildAndCleanup || exit 1
+
+# done
+exit 0
